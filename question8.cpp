fe44a9e2ba86569cdfd7f901238cc771de41c211
FerrantA31 <58517126+FerrantA31@users.noreply.github.com>
2020-09-11 15:20:51 +0200

    Create question8.cpp
    Q8

---
+++ question8.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nmbrU;
+
+	cout << "Choisi un nombre";
+
+	cin >>nmbrU;
+
+	if (nmbrU < 0)
+	{
+		cout << "Le nombre choisi est negatif";
+	}
+	else if (nmbrU > 0)
+	{
+		cout << "Le nombre choisi est NUL";
+	}
+	else (nmbrU == 0)
+
+		; {
+		cout << "Le nombre choisi est positif";
+	}
+	
+		
+	
+	
+	
+
+
+
+
+
+
+
+
+}
